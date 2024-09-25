463687d74310fc92190fb26a18a71bf0f5ef5914
Simone Astolfi <simoasto95@gmail.com>
2024-09-25 14:36:55 +0200

    fixed multipage

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     authenticator, name, authentication_status, username = login()
     # Handle authentication status
     if authentication_status:
-        main()
+        st.switch_page("Pages/primary.py")
         authenticator.logout('Log out')
     elif authentication_status == False:
         st.error('Username or password is incorrect')
