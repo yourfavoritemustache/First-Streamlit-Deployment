947c9b219cd27cbfda272246647a15b0156d3729
Simone Astolfi <simoasto95@gmail.com>
2024-09-25 15:01:51 +0200

    fixed multipage

--- pages/primary.py
+++ pages/primary.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-if 'authentication_status' not in st.session_state:
+if 'authentication_status' not in st.session_state[]:
     st.switch_page("app.py")
 else:
     st.title('Main Page')
