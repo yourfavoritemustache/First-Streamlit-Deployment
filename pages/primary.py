01528f6219c545493d59293aedfda37393850b29
Simone Astolfi <simoasto95@gmail.com>
2024-09-25 16:06:58 +0200

    fixed multipage

--- pages/primary.py
+++ pages/primary.py
@@ -8,7 +8,7 @@ import streamlit as st
 #     st.write('Private page bitch')
 #     st.session_state.authentication_status
     
-if st.session_state.authentication_status == 'None' | st.session_state.authentication_status == False:
+if (st.session_state.authentication_status == 'None') or (st.session_state.authentication_status == False):
     st.switch_page("app.py")
 else:
     st.title('Main Page')
