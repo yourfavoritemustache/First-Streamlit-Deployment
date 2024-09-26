de235b01a93f344ae5c94fea4a36c256507c82fe
Simone Astolfi <simoasto95@gmail.com>
2024-09-26 10:07:19 +0200

    log out button position

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     
     if authentication_status:
         pag_layout(username)
-        if st.sidebar.button('Logout'):
+        with st.sidebar.button('Logout'):
             authenticator.logout()
     elif authentication_status == False:
         st.error('Username or password is incorrect')
